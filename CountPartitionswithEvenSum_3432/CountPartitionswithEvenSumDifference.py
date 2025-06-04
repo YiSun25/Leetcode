ebacac0116eac9437780ff40c3c6c6bf09925784
Yi Sun <yi.sun@rwth-aachen.de>
2025-06-04 21:08:56 +0200

    Count Partitions with Even Sum Difference

---
+++ CountPartitionswithEvenSum_3432/CountPartitionswithEvenSumDifference.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countPartitions(self, nums: list[int]) -> int:
+        return 0 if sum(nums) % 2 else len(nums) - 1
+    
+solution = Solution()
+input = [2, 5, 6, 1, 5]
+print(solution.countPartitions(input))
